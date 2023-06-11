1b890c2dd33fff003a22679ec349934eb7f72ded
r.hosoya <r.hosoya@core-tech.jp>
2023-06-12 06:33:02 +0900

    ⚡️ フッター修正

--- front/src/layouts/Footer.tsx
+++ front/src/layouts/Footer.tsx
@@ -1,5 +1,13 @@
+import { Center, Flex } from "@chakra-ui/react";
+
 const Footer = () => {
-	return <></>;
+	return (
+		<>
+		<Center bg='cor.100' h='50px' color='white'>
+			(C) 2023 CoreTech
+		</Center>
+		</>
+	);
 };
 
 export default Footer;
