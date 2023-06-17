28a16ca813482f64940de1d9092b43424a0a391e
r.hosoya <r.hosoya@core-tech.jp>
2023-06-17 13:53:13 +0900

    axios導入とAPI情報をfetch

--- front/src/hooks/useGetShopsData.ts
+++ front/src/hooks/useGetShopsData.ts
@@ -1,10 +1,27 @@
+import axios from 'axios';
 import { useCallback, useState } from 'react';
-import { Shops } from '../db/Shops';
 
 const useGetShopsData = () => {
-	const [shopsData, setShopsData] = useState([]);
-	const GetShopsData = useCallback(() => {
-		const shopsData = Shops;
-	}, []);
-	return { shopsData };
+	// API情報を定義
+	const API_KEY = 'f58be42c8cff0bc1';
+	const URL = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' + API_KEY + '&format=json' + '&large_area=Z011';
+
+	const [shops, setShops] = useState([]);
+
+	const getShopsData = useCallback(
+		() => {
+			axios
+			.get(URL)
+			.then((res: any) => {
+				const fetchedData = res.data.results.shop;
+				setShops(fetchedData);
+				console.log(res.data.results.shop);
+			})
+			.catch((err: any) => {
+				console.error(err);
+			});
+		}, []);
+	return { getShopsData , shops};
 };
+
+export default useGetShopsData;
--- front/src/pages/Top.tsx
+++ front/src/pages/Top.tsx
@@ -2,81 +2,56 @@ import {
 	Box,
 	Center,
 	Heading,
+	Image,
 	SimpleGrid,
 	Stack,
 } from '@chakra-ui/react';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Shop from '../components/Shop';
-import useGetShopData from '../hooks/useGetShopData';
+import useGetShopsData from '../hooks/useGetShopsData';
 
 const Top = () => {
-	const { getShopData, shopData } = useGetShopData();
-	useEffect(() => getShopData(0), []);
-
-	const navigate = useNavigate();
-	const onClickSearchButton = () => {
-		navigate('shops');
-	};
+	const { getShopsData, shops } = useGetShopsData();
+	useEffect(() => getShopsData(), []);
 
 	return (
+		<>
 		<Box p="20px">
+			<hr />
 			<Stack>
-				<Heading as="h2">これはTOPページです！</Heading>
-				<hr />
-				<Heading as="h3" size="lg">カテゴリ別に検索</Heading>
-				<SimpleGrid columns={[2, null, 5]} spacing='40px'>
-					<Center bg='cor.200' height='80px'>
-						中華
-					</Center>
-					<Center bg='cor.200' height='80px'>
-						麺類
-					</Center>
-					<Center bg='cor.200' height='80px'>
-						定食
-					</Center>
-					<Center bg='cor.200' height='80px'>
-						洋食
-					</Center>
-					<Center bg='cor.200' height='80px'>
-						アメリカ料理
-					</Center>
-				</SimpleGrid>
-				<Heading as="h3" size="lg">本日のオススメの店舗</Heading>
-				<SimpleGrid columns={2} spacing={10} >
-					<Box>
-						<Shop shopData={shopData}/>
-					</Box>
-					<Box>
-						<Shop shopData={shopData}/>
-					</Box>
-					<Box>
-						<Shop shopData={shopData}/>
-					</Box>
-					<Box>
-						<Shop shopData={shopData}/>
-					</Box>
-					<Box>
-						<Shop shopData={shopData}/>
-					</Box>
-					<Box>
-						<Shop shopData={shopData}/>
-					</Box>
-					<Box>
-						<Shop shopData={shopData}/>
-					</Box>
-					<Box>
-						<Shop shopData={shopData}/>
-					</Box>
-					<Box>
-						<Shop shopData={shopData}/>
-					</Box>
-					<Box>
-						<Shop shopData={shopData}/>
-					</Box>
-				</SimpleGrid>
+				<Box my={4}>
+					<Heading as="h3" size="lg" m={2}>カテゴリ別に検索</Heading>
+					<SimpleGrid columns={[2, null, 5]} spacing='40px'>
+						<Center bg='cor.200' height='80px'>
+							中華
+						</Center>
+						<Center bg='cor.200' height='80px'>
+							麺類
+						</Center>
+						<Center bg='cor.200' height='80px'>
+							定食
+						</Center>
+						<Center bg='cor.200' height='80px'>
+							洋食
+						</Center>
+						<Center bg='cor.200' height='80px'>
+							アメリカ料理
+						</Center>
+					</SimpleGrid>
+				</Box>
+				<Box m={4}>
+					<Heading as="h3" size="lg" m={2}>本日のオススメの店舗</Heading>
+					<SimpleGrid columns={2} spacing={10} >
+						{shops.map((shop, index) => (
+							<Box key={index}>
+								<Shop shop={shop}/>
+							</Box>
+						))}
+					</SimpleGrid>
+				</Box>
 			</Stack>
 		</Box>
+		</>
 	);
 };
 
