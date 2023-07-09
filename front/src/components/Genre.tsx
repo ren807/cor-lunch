98d0265851950a06e07903e0bf56395999fa6e68
r.hosoya <r.hosoya@core-tech.jp>
2023-07-09 17:10:08 +0900

    fix:ジャンルコンポーネント改修

--- front/src/components/Genre.tsx
+++ front/src/components/Genre.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import useGetGenre from "../hooks/useGetGenre";
@@ -13,60 +13,29 @@ const Genre = () => {
 
 	return (
 		<>
-			<Heading as="h3" size="lg" m={2} textAlign={{ base: "center", md: "left" }}>カテゴリ別に検索 🔍︎</Heading>
-			<SimpleGrid columns={[1, 2, 4]} spacing='40px' my={4}>
+			<Heading as="h3" size="lg" my={8} textAlign={{ base: "center", md: "left" }}>カテゴリ別に検索 🔍︎</Heading>
+			<Grid templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(8, 1fr)", lg: "repeat(12, 1fr)" }} gap={6} >
 				{genresData?.map((genre, index) => (
-					<Link 
-						key={index}
-						to={genre.path}
-						state={{ genre: genre.name }}
-						onClick={() => setCurrentGenre(genre.path)}
-					>
-						<Box
-							position='relative'
-							color="black"
-							height='120px'
-							display='flex'
-							justifyContent='center'
-							alignItems='center'
-						>
-							<Box
-								position='absolute'
-								top='0'
-								left='0'
-								right='0'
-								bottom='0'
-								style={{
-									backgroundImage: `url(${genre.photo})`,
-									backgroundSize: 'cover',
-									backgroundPosition: 'right',
-									opacity: currentGenre === genre.path ? '0.5' : '1'
-								}}
-							/>
-							<Text
-								color="black"
-								fontSize="xl"
-								fontWeight="bold"
-							>
-								選択中
-							</Text>
-							<Text
-								position='absolute'
-								right='0px'
-								bottom='5px'
-								bg='cor.100'
-								color='white'
-								px='25px'
-								fontSize='sm'
-								borderRadius='10px'
-								borderRightRadius='0px'
-							>
-								{genre?.name}
-							</Text>
-						</Box>
-					</Link>
+					<GridItem colSpan={{ base: 1, md: 4, lg: 3 }} >
+						<Link key={index} to={genre.path} state={{ genre: genre.name }} onClick={() => setCurrentGenre(genre.path)} >
+							<Box position="relative" color="black" height="120px" display="flex" justifyContent="center" alignItems="center" >
+								<Box position="absolute" top="0" left="0" right="0" bottom="0"
+									style={{
+										backgroundImage: `url(${genre.photo})`,
+										backgroundSize: "cover",
+										backgroundPosition: "right",
+										opacity: currentGenre === genre.path ? "0.5" : "1"
+									}}
+								/>
+								<Text color="black" fontSize="xl" fontWeight="bold">選択中</Text>
+								<Text position="absolute" right="0px" bottom="5px" bg="cor.100" color="white" px="25px" fontSize="sm" borderRadius="10px" borderRightRadius="0px" >
+									{genre?.name}
+								</Text>
+							</Box>
+						</Link>
+					</GridItem>
 				))}
-			</SimpleGrid>
+			</Grid>
 		</>
 	);
 };
--- front/src/pages/Top.tsx
+++ front/src/pages/Top.tsx
@@ -1,15 +1,14 @@
 import {
 	Box,
 	Heading,
-	Stack,
 	Wrap,
 	WrapItem,
-} from '@chakra-ui/react';
-import { useEffect } from 'react';
+} from "@chakra-ui/react";
+import { useEffect } from "react";
 
-import Genre from '../components/Genre';
-import Shop from '../components/Shop';
-import useGetShopsData from '../hooks/useGetShopsData';
+import Genre from "../components/Genre";
+import Shop from "../components/Shop";
+import useGetShopsData from "../hooks/useGetShopsData";
 
 const Top = () => {
 	const { getShopsData, shopsData } = useGetShopsData();
@@ -17,27 +16,18 @@ const Top = () => {
 
 	return (
 		<>
-			<Box mx={{ base: 5, md: 20}} pb={5}>
-				<Stack>
-					{/* カテゴリ一覧 */}
-					<Box my={4}>
-						<Genre />
-					</Box>
-					{/* おすすめ一覧 */}
-					<Box mb={4} width="100%">
-						<Heading as="h3" size="lg" mt={2} mb={5} textAlign={{ base: "center", md: "left" }}>
-							本日のオススメの店舗 🎉
-						</Heading>
-						{/* 店舗カード */}
-						<Wrap spacing="30px">
-							{shopsData?.map((shop, index) => (
-								<WrapItem key={index} m="auto" shadow="md">
-									<Shop {...shop}/>
-								</WrapItem>
-							))}
-						</Wrap>
-					</Box>
-				</Stack>
+			<Box my={5} px={5} mx="auto" maxWidth="1600px">
+				{/* カテゴリ一覧 */}
+				<Genre />
+				{/* おすすめ一覧 */}
+				<Heading as="h3" size="lg" my={8} textAlign={{ base: "center", md: "left" }}>本日のオススメの店舗 🎉</Heading>
+				<Wrap spacing="30px">
+					{shopsData?.map((shop, index) => (
+						<WrapItem key={index} m="auto" shadow="md">
+							<Shop {...shop}/>
+						</WrapItem>
+					))}
+				</Wrap>
 			</Box>
 		</>
 	);
