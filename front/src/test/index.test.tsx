c8b3517026390b7a7cd4f954b92c97e1953c25df
r.hosoya <r.hosoya@core-tech.jp>
2023-07-22 12:25:23 +0900

    Footerのテストコード実装

---
+++ front/src/test/index.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import Footer from '../layouts/Footer';
+
+describe('Footer component', () => {
+	test('renders Footer and checks text content', () => {
+		render(<Footer />);
+		const footerElement = screen.getByText('(C) 2023 CoreTech');
+		expect(footerElement).toBeInTheDocument();
+	});
+});
